ee31fce31208ff55823d7b041361faf19c9e0c96
Anton Karatkevich <antonkaratkevich@192.168.2.53>
2019-08-26 11:33:54 +0300

    initial commit

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,104 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getFilterSchemaFor,
+  getModelSchemaRef,
+  getWhereSchemaFor,
+  patch,
+  put,
+  del,
+  requestBody,
+} from '@loopback/rest';
+import { Users } from '../models';
+import { UsersRepository } from '../repositories';
+
+export class UserController {
+  constructor(
+    @repository(UsersRepository)
+    public usersRepository: UsersRepository,
+  ) { }
+
+  @post('/form', {
+    responses: {
+      '200': {
+        description: 'Users model instance',
+        content: { 'application/json': { schema: getModelSchemaRef(Users) } },
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Users, { exclude: ['id', 'description'] }),
+        },
+      },
+    })
+    users: Omit<Users, 'id'>,
+  ): Promise<Users> {
+    return this.usersRepository.create(users);
+  }
+
+  @get('/api', {
+    responses: {
+      '200': {
+        description: 'Array of Users model instances',
+        content: {
+          'application/json': {
+            schema: { type: 'array', items: getModelSchemaRef(Users) },
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.object('filter', getFilterSchemaFor(Users)) filter?: Filter<Users>,
+  ): Promise<Users[]> {
+    return this.usersRepository.find(filter);
+  }
+
+  @get('/user/{id}', {
+    responses: {
+      '200': {
+        description: 'Users model instance',
+        content: { 'application/json': { schema: getModelSchemaRef(Users) } },
+      },
+    },
+  })
+  async findById(@param.path.number('id') id: number): Promise<Users> {
+    return this.usersRepository.findById(id);
+  }
+
+  @put('/edit/user/{id}', {
+    responses: {
+      '204': {
+        description: 'Users PUT success',
+      },
+    },
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() users: Users,
+  ): Promise<void> {
+    await this.usersRepository.replaceById(id, users);
+  }
+
+  @del('/delete', {
+    responses: {
+      '204': {
+        description: 'Users DELETE success',
+      },
+    },
+  })
+  async deleteById(@requestBody() id: number): Promise<void> {
+    await this.usersRepository.deleteById(id);
+  }
+}
---
+++ src/models/users.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {}})
+export class Users extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  firstname?: string;
+
+  @property({
+    type: 'string',
+  })
+  lastname?: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+
+  constructor(data?: Partial<Users>) {
+    super(data);
+  }
+}
+
+export interface UsersRelations {
+  // describe navigational properties here
+}
+
+export type UsersWithRelations = Users & UsersRelations;
